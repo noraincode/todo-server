41b585b9a329f16385ff36f8b3829d3832c03f1f
Evans <norain4u@yeah.net>
2024-02-02 15:37:16 +0800

    :wrench: Update cors config

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './nest/interceptors/response.interceptor';
 import { HttpExceptionFilter } from './nest/filters/http-exception.filter';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  app.enableCors({
-    origin: 'https://todo-app-psi-roan.vercel.app', // Change to http://localhost:3000 while debug
-    methods: 'GET,PATCH,POST,DELETE', // 允许的 HTTP 请求方法
-    allowedHeaders: 'Content-Type, Accept', // 允许的 HTTP 请求头
-    credentials: true, // 是否允许发送 Cookie
-  });
+  const corsOptions: CorsOptions = {
+    origin: ['https://todo-app-psi-roan.vercel.app', 'http://localhost:3000 '],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Length', 'X-Custom-Header'],
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(5001);
 }
 bootstrap();
