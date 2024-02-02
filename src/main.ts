fe6fd6b8b77b56f7c522c784b2d6dc0414babccc
Evans <norain4u@yeah.net>
2024-02-02 15:29:35 +0800

    :wrench: Enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,8 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  const allowedOrigins = [
-    'http://localhost:3000',
-    'https://todo-app-psi-roan.vercel.app',
-  ];
-
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: 'https://todo-app-psi-roan.vercel.app', // Change to http://localhost:3000 while debug
     methods: 'GET,PATCH,POST,DELETE', // 允许的 HTTP 请求方法
     allowedHeaders: 'Content-Type, Accept', // 允许的 HTTP 请求头
     credentials: true, // 是否允许发送 Cookie
